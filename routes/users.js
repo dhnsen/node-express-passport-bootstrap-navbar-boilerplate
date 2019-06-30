3f5d8d6a2d1ae1e92d000b31ce2f8b76e28d37e6
Dan H <spag1024@hotmail.com>
2019-06-30 07:50:13 -0500

    finished registration and password hashing

--- routes/users.js
+++ routes/users.js
@@ -58,8 +58,18 @@ router.post('/register', (req, res) => {
                         email,
                         password
                     });
-                    console.log('new user');
-                    res.send('user does not exist yet');
+                    //hash password
+                    bcrypt.genSalt(10, (err, salt) => 
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        //save user
+                        newUser.save()
+                        .then( user => {
+                            res.redirect('/login');
+                        })
+                        .catch(err => console.log(err));
+                    }))
                 }
             });
     }
