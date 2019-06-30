84679e8c83b931275f735e4c65826c4188593f84
Dan H <spag1024@hotmail.com>
2019-06-30 14:26:29 -0500

    wrapped authentication, messages and dashboard route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 // Welcome page
 router.get('/', (req, res) => res.render('welcome'));
 
 //Dashboard
-router.get('/dashboard', (req, res) => res.render('dashboard'));
+router.get('/dashboard', ensureAuthenticated, (req, res) => 
+res.render('dashboard', {
+    name: req.user.name
+}));
 
 module.exports = router;
