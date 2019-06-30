bc692f46ad9dde5c4f32ede6fab407c1e8cd1a14
Dan H <spag1024@hotmail.com>
2019-06-30 14:14:38 -0500

    finished authentication and logout route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// Welcome page
 router.get('/', (req, res) => res.render('welcome'));
+
+//Dashboard
+router.get('/dashboard', (req, res) => res.render('dashboard'));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -78,8 +78,19 @@ router.post('/register', (req, res) => {
 });
 
 // Login Handle
-router.post('/login', (req, res) => {
-
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '../dashboard',
+        failureRedirect: '../users/login',
+        failureFlash: true
+    })(req, res, next);
 });
+
+// Logout Handle
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+})
 
 module.exports = router;
